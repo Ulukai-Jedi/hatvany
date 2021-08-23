88ac0f0a7c48f8ea52fd438e3cda6469460e53d1
ulukai <gergelybrunda@gmail.com>
2021-08-23 18:29:46 +0200

    Add project files.

---
+++ hatvany/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+unsigned long long int hatvany(const int a, const int b) {
+	unsigned long long int eredmeny = 1UL;
+	for (int i = 0; i < b; i++) {
+		 eredmeny *= a;
+	}
+	return eredmeny;
+}
+
+
+int main() {
+
+	int a = 0;
+	int b = 0;
+	std::cout << "Kerem a szamot ";
+	std::cin >> a;
+	std::cout << "Kerem a kitevot ";
+	std::cin >> b;
+	std::cout << hatvany(a,b);
+
+	return 0;
+}
